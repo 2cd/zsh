23beda50f2dfac2c1c1de555c9b9625363a55279
2moe <25324935+2moe@users.noreply.github.com>
2020-10-15 11:16:09 +0800

    less -m -RFeQ

--- config/zshrc.zsh
+++ config/zshrc.zsh
@@ -107,17 +107,25 @@ else
 fi
 [[ ! $(command -v tmoe) ]] || alias t=tmoe
 ######
-set_bat_paper_variable(){
+set_bat_paper_variable() {
     local CAT_BIN_FILE=$(whereis cat 2>/dev/null | awk '{print $2}')
     alias lcat=${CAT_BIN_FILE}
-    export BAT_PAGER="less -m -RFeQ"
+    if [ -e "/etc/os-release" ]; then
+        if ! egrep -q 'bionic|Bionic Beaver|Eoan Ermine|buster|stretch|jessie' "/etc/os-release"; then
+            export BAT_PAGER="less -m -RFeQ"
+        fi
+    elif [[ $(uname -o) = 'Android' ]]; then
+        export BAT_PAGER="less -m -RFeQ"
+    else
+        export BAT_PAGER="less"
+    fi
 }
 if [ $(command -v batcat) ]; then
     set_bat_paper_variable
     alias cat='batcat' #bat是cat的替代品，支持多语言语法高亮。支持自动分页，对于大文本，以 less 命令输出，则可使用类似 vim 的快捷键移动光标。
 elif [ $(command -v bat) ]; then
     set_bat_paper_variable
-    alias cat='bat'     #输q退出bat的页面视图，you can type q to quit bat.
+    alias cat='bat' #输q退出bat的页面视图，you can type q to quit bat.
 fi
 ########
 #########
